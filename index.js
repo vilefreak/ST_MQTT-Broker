5308e2706774b16e659896fbc2abd212d9c2cfd3
vilefreak <a01424326@tec.mx>
2024-09-11 12:36:30 -0600

    Creada la base de recepción/subscriber

---
+++ index.js
@@ -0,0 +1,34 @@
+const mqtt = require('mqtt')
+
+const protocol = 'mqtt'
+const host = '3.82.100.202'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `${protocol}://${host}:${port}`
+
+const client = mqtt.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: 'steve',
+    password: 'josexo',
+    reconnectPeriod: 1000,
+  })
+  
+  client.on('connect', () => {
+    console.log('Connected')
+  })
+  
+  const topic = 'test'
+
+  client.on('connect', () => {
+    console.log('Connected')
+    client.subscribe([topic], () => {
+      console.log(`Subscribe to topic '${topic}'`)
+    })
+  })
+
+  client.on('message', (topic, payload) => {
+    console.log('Received Message:', topic, payload.toString())
+  })
